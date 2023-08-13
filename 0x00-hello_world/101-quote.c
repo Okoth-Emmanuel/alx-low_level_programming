a5a1e192df3cd24bc38019963adb7df97cdb39ea
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2023-08-12 18:44:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * a c program that prints a line to the error
+ * return 1 (success)
+ */
+int main(void){
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
+	return (1);
+}
