d6e5f4c252a661f3e72842486f8aa606350c0170
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2023-08-19 16:15:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
 #include "main.h"
-#include <unistd.h>
 /**
- * _putchar - writes character z to stdout
- * @z: character to be printed
- *
- * Return: 1 On success
- * On error, returnes -1, and errno is set appropriately.
- */
-int _putchar(char z)
+* main - prints _putchar
+* Return: 0 on success
+*/
+int main(void)
 {
-return (write(1, &z, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,16 @@
 #ifndef main_h
 #define main_h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int a);
-int _isalpha(int a);
 int print_sign(int n);
 int _abs(int);
+int _islower(int c);
+int _isalpha(int c);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
 void print_to_98(int n);
+int add(int, int);
+void print_times_table(int n);
+#endif
