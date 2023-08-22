ddbf0568e599b73626d7b0adb4dc65dab48a9344
Okoth-Emmanuel <emmanuel.okoth7@gmail.com>
2023-08-22 06:42:35 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,8 +2,7 @@
 /**
 * main - Lists all the natural numbers below 1024 (excluded),
 * that are multiples of 3 or 5 followed by a new line.
-**
-Return: 0 on success.
+* Return: always 0.
 */
 int main(void)
 {
